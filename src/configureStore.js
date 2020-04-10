2507f40858cfa1134b4ec2e190e6ecb1b5ed5f12
David Hong <david.hong@terasquad.com>
2020-04-10 12:49:08 +0800

    add redux persist

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,27 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}    
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default function configureStore(initialState) {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-  return createStore(
-    rootReducer,
+  const store = createStore(
+    persistedReducer,
     initialState,
     composeEnhancers(applyMiddleware(thunk))
   );
+
+  persistStore(store);
+  
+  return store
 }
